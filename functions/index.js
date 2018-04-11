40d6e60d3ce27bec2a0d875473b584cd6d9d59a4
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-04-12 00:38:35 +0530

    first try

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+function getDetails(id,value,ref)
+{
+    const sensor = id;
+    return{
+        status: data.status,
+        start: data.start
+    };
+
+}
+
+
+
+exports.pushTransaction = functions.database.ref('/Sensors/{id}').onUpdate(event=>{
+    const rootRef = event.data.ref.parent.parent;
+    const countRef = rootRef.child('count');
+    
+
+    const values = getDetails(event.params.id, event.data.val(), event.data.ref);
+  console.log(values);
+
+
+});
