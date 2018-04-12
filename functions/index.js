6c9a083b2a8d2000bda97deedf485b4bf1e1a3df
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-04-12 07:06:12 +0530

    need help

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,9 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 
-function getDetails(id,value,ref)
+function getDetails(id, value, ref)
 {
+    const data = value;
     const sensor = id;
     return{
         status: data.status,
@@ -16,12 +17,22 @@ function getDetails(id,value,ref)
 
 
 exports.pushTransaction = functions.database.ref('/Sensors/{id}').onUpdate(event=>{
+
+    console.log(event.data);
+    console.log("inside function");
     const rootRef = event.data.ref.parent.parent;
     const countRef = rootRef.child('count');
     
+    console.log(event.data);
 
     const values = getDetails(event.params.id, event.data.val(), event.data.ref);
-  console.log(values);
+    console.log(values);
+
+
 
 
 });
+
+
+
+// Inside Sensor.....{id}.....if status is now 1,....increment count......if 0 decrement count
