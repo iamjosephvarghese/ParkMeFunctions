80effe2401034849a1d34c506171bacf1c382bb2
Joseph Varghese <josephvarghese.rms@gmail.com>
2018-04-12 11:53:43 +0530

    combined functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+const DialogflowApp = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
 
 
 function getDetails(id, value, ref)
@@ -12,6 +18,40 @@ function getDetails(id, value, ref)
         start: data.start
     };
 }
+
+
+const database = admin.database().ref('Sensors/');
+const countRef = database.child('count');
+
+exports.fetchCount = functions.https.onRequest((request,response)=>{
+    console.log('headers: ' + JSON.stringify(request.headers));
+    console.log('body: ' + JSON.stringify(request.body));
+
+    const assistant = new DialogflowApp({request: request, response: response});
+
+    let actionMap = new Map();
+    actionMap.set('rush_check', rush_check);
+    assistant.handleRequest(actionMap);
+
+
+    function rush_check(assistanat){
+        console.log('inside countFunc');
+
+        countRef.once('value',snap=>{
+            const carCount = snap.val();
+            console.log(`carCount: ${carCount}`);
+
+            const speech = `<speak>
+            No of cars is  ${snap.val()}
+            </speak>`;
+
+            assistant.ask(speech)
+
+        });
+
+    }
+});
+
 
 
 
@@ -29,7 +69,3 @@ exports.pushTransaction = functions.database.ref('/Sensors/{id}/status').onUpdat
         return (current||0)+increment;
     });
 });
-
-
-
-// Inside Sensor.....{id}.....if status is now 1,....increment count......if 0 decrement count
